87e1ffece980d8e24934416fe7354471c1469fc9
syun77 <kenmomomomomo@gmail.com>
2014-07-09 21:36:31 +0900

    fixed #10 スピードゲージを見やすくする

--- source/ui/HUD.hx
+++ source/ui/HUD.hx
@@ -1,4 +1,6 @@
 package ui;
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
 import SpeedController;
 import token.Player;
 import flixel.util.FlxStringUtil;
@@ -10,6 +12,56 @@ import flixel.text.FlxText;
 import flixel.group.FlxGroup;
 
 /**
+ * スピードバー
+ **/
+class SpeedBar extends FlxGroup {
+
+    private var _frame:FlxSprite; // 外枠
+    private var _frame2:FlxSprite;
+    private var _meter:FlxSprite; // メーター
+
+    private var _x:Float; // X座標
+    private var _y:Float; // Y座標
+    private var _width:Int; // 幅
+    private var _height:Int; // 高さ
+
+    public function new(px:Float, py:Float, w:Int, h:Int) {
+        super();
+        _x = px;
+        _y = py;
+        _width = w;
+        _height = h;
+
+        _frame = new FlxSprite(_x-2, _y-2);
+        _frame.makeGraphic(_width+4, _height+4, FlxColor.WHITE);
+        _frame.scrollFactor.set(0, 0);
+        _frame2 = new FlxSprite(_x, _y);
+        _frame2.makeGraphic(_width, _height, FlxColor.BLACK);
+        _frame2.scrollFactor.set(0, 0);
+
+        _meter = new FlxSprite(_x, _y);
+        _meter.makeGraphic(_width, _height, FlxColor.GREEN);
+        _meter.scrollFactor.set(0, 0);
+
+        this.add(_frame);
+        this.add(_frame2);
+        this.add(_meter);
+    }
+
+    public function getY():Float { return _y; }
+
+    public function setRatio(per:Float):Void {
+        if(per < 0) { per = 0; }
+        if(per > 1) { per = 1; }
+
+        _meter.scale.x = per;
+        _meter.x = _x - (1 - per) * _width/2;
+    }
+
+
+}
+
+/**
  * Head up display.
  **/
 class HUD extends FlxGroup {
@@ -19,7 +71,7 @@ class HUD extends FlxGroup {
     private static inline var SPEEDBAR_POS_X = 32;
     private static inline var SPEEDBAR_POS_Y1 = 16;
     private var SPEEDBAR_POS_Y2 = FlxG.height-16;
-    private var SPEEDBAR_WIDTH = FlxG.height*0.75;
+    private var SPEEDBAR_WIDTH = FlxG.width*0.75;
     private var SPEEDBAR_HEIGHT = 8;
     private var SPEEDTXT_POS_X:Float;
     // スピードテキスト
@@ -41,8 +93,8 @@ class HUD extends FlxGroup {
     private var _bIncTime:Bool = false; // 経過時間の増加フラグ
 
     // ゲージ
-    private var _barSpeed:FlxBar;
     private var _barDistance:FlxBar;
+    private var _barSpeed:SpeedBar;
 
     private var _objs:Array<FlxObject>;
 
@@ -70,24 +122,31 @@ class HUD extends FlxGroup {
         var y1 = FlxG.height-16;
         var dy = 12;
         y1 += dy;
-        _barSpeed = new FlxBar(SPEEDBAR_POS_X, SPEEDBAR_POS_Y2, FlxBar.FILL_LEFT_TO_RIGHT, cast SPEEDBAR_WIDTH, SPEEDBAR_HEIGHT);
-        _txtSpeed = new FlxText(SPEEDTXT_POS_X, _barSpeed.y, width);
+        // スピードゲージ
+        _barSpeed = new SpeedBar(SPEEDBAR_POS_X, SPEEDBAR_POS_Y2, cast SPEEDBAR_WIDTH, SPEEDBAR_HEIGHT);
+        this.add(_barSpeed);
+        _txtSpeed = new FlxText(SPEEDTXT_POS_X, _barSpeed.getY(), width);
         _txtDistance = new FlxText(x, y2, width);
         _txtLevel = new FlxText(-8, y1-24, width);
         _txtLevel.text = Reg.getLevelName();
         y2 += dy;
+
+        // 残り距離
         _barDistance = new FlxBar(x, y2-2, FlxBar.FILL_LEFT_TO_RIGHT, cast FlxG.width/3, 2);
         _txtTime = new FlxText(x, y2, width);
         _txtTime.text = "Time: " + FlxStringUtil.formatTime(0, true);
         y2 += dy;
+
+        // レベル
         _txtLevel.alignment = "right";
+
+        // コンボ数
         _txtCombo = new FlxText(FlxG.width-72, y2, 64);
         _txtCombo.alignment = "center";
         _txtCombo2 = new FlxText(FlxG.width-56, y2+24, 80);
         _txtCombo2.text = "combo";
         _txtCombo2.visible = false;
 
-        _objs.push(_barSpeed);
         _objs.push(_barDistance);
         _objs.push(_txtTime);
         _objs.push(_txtSpeed);
@@ -154,7 +213,7 @@ class HUD extends FlxGroup {
         _txtSpeed.text = "Speed: " + Math.floor(_player.velocity.x);
         _txtDistance.text = "Distance: " + Math.floor(_player.x/10) + "/" + Math.floor(_goal/10);
 
-        _barSpeed.percent = 100*_player.velocity.x / SpeedController.MAX;
+        _barSpeed.setRatio(_player.velocity.x / SpeedController.MAX);
         _barDistance.percent = 100*_player.x / _goal;
 
         if(_txtCombo.size > 16) {
