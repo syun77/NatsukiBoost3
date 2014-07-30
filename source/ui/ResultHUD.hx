bbb46c680637aaf54d7c326ce4fa49339af0a52a
syun77 <kenmomomomomo@gmail.com>
2014-07-30 21:59:12 +0900

    リザルト演出実装中

--- source/ui/ResultHUD.hx
+++ source/ui/ResultHUD.hx
@@ -1,4 +1,7 @@
 package ui;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxObject;
 import flixel.group.FlxGroup;
 
 /**
@@ -6,11 +9,34 @@ import flixel.group.FlxGroup;
  **/
 class ResultHUD extends FlxGroup {
 
+    private var _objs:Array<FlxObject>;
+    // ゲームオブジェクト
+    private var _bowl:FlxSprite;
+    private var _bowl2:FlxSprite;
+    private var _panel:FlxSprite;
+
     /**
      * コンストラクタ
      **/
     public function new() {
         super();
+        _objs = new Array<FlxObject>();
+
+        _bowl = new FlxSprite(0, 0);
+        _bowl.loadGraphic("assets/images/result/boul_oku.png");
+        _bowl2 = new FlxSprite(0, 0);
+        _bowl2.loadGraphic("assets/images/result/boul_mae.png");
+        _panel = new FlxSprite(FlxG.width/2, FlxG.height/2+40);
+//        _panel.loadGraphic("assets/images/result/scoer_fream.png");
+//        _panel.x -= _panel.width/2;
+
+        _objs.push(_bowl);
+        _objs.push(_bowl2);
+//        _objs.push(_panel);
+        for(obj in _objs) {
+            obj.scrollFactor.set(0, 0);
+            this.add(obj);
+        }
     }
 
     public function isEnd():Bool {
