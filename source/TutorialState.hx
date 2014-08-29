52c87b4867136ff27c62b610b11efa3753f3bd12
syun77 <kenmomomomomo@gmail.com>
2014-08-29 10:08:20 +0900

    チュートリアル表示方法を修正

--- source/TutorialState.hx
+++ source/TutorialState.hx
@@ -14,7 +14,7 @@ import flixel.FlxState;
 class TutorialState extends FlxState {
     private var _nPage:Int = 0; // 現在のページ番号
     private var _btn:FlxButton;
-    private var _spr:FlxSprite;
+    private var _spr:FlxSprite = null;
 
     override public function create() {
         super.create();
@@ -35,9 +35,7 @@ class TutorialState extends FlxState {
         FlxTween.tween(fade, {alpha:0.7}, 1, {ease:FlxEase.expoOut});
         this.add(fade);
 
-        _spr = new FlxSprite();
         _cbBtn();
-        this.add(_spr);
 
         _btn = new FlxButton(FlxG.width-80, FlxG.height-24, "Next", _cbBtn);
         this.add(_btn);
@@ -56,15 +54,23 @@ class TutorialState extends FlxState {
             _btn.text = "End";
         }
         var path = 'assets/images/tutorial/${TextUtil.fillZero(_nPage, 3)}.png';
+
+        if(_spr != null) {
+            FlxTween.tween(_spr, {x:-_spr.width}, 1, {ease:FlxEase.expoOut});
+            FlxTween.tween(_spr, {alpha:0}, 1, {ease:FlxEase.expoOut});
+        }
+
+        _spr = new FlxSprite();
         _spr.loadGraphic(path);
-        _spr.alpha = 0;
+        _spr.x = _spr.width;
+        this.add(_spr);
     }
 
     override public function update():Void {
         super.update();
 
-        if(_spr.alpha < 1) {
-            _spr.alpha += 0.02;
+        if(_spr.x > 0) {
+            _spr.x *= 0.9;
         }
     }
 }
